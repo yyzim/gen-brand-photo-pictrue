a6808460877c0dd4f6061c55dc1c9fba4dd61d1d
yangyizhou <yizhouyang@yeah.net>
2023-02-12 11:16:47 +0800

    Updates

---
+++ pkg/gen_brand_photo_pictrue.d.ts
@@ -0,0 +1,43 @@
+/* tslint:disable */
+/* eslint-disable */
+/**
+*/
+export function run(): void;
+/**
+* @param {Uint8Array} raw
+* @returns {any}
+*/
+export function get_exif(raw: Uint8Array): any;
+
+export type InitInput = RequestInfo | URL | Response | BufferSource | WebAssembly.Module;
+
+export interface InitOutput {
+  readonly memory: WebAssembly.Memory;
+  readonly get_exif: (a: number, b: number) => number;
+  readonly run: () => void;
+  readonly __wbindgen_malloc: (a: number) => number;
+  readonly __wbindgen_free: (a: number, b: number) => void;
+  readonly __wbindgen_realloc: (a: number, b: number, c: number) => number;
+  readonly __wbindgen_start: () => void;
+}
+
+export type SyncInitInput = BufferSource | WebAssembly.Module;
+/**
+* Instantiates the given `module`, which can either be bytes or
+* a precompiled `WebAssembly.Module`.
+*
+* @param {SyncInitInput} module
+*
+* @returns {InitOutput}
+*/
+export function initSync(module: SyncInitInput): InitOutput;
+
+/**
+* If `module_or_path` is {RequestInfo} or {URL}, makes a request and
+* for everything else, calls `WebAssembly.instantiate` directly.
+*
+* @param {InitInput | Promise<InitInput>} module_or_path
+*
+* @returns {Promise<InitOutput>}
+*/
+export default function init (module_or_path?: InitInput | Promise<InitInput>): Promise<InitOutput>;
